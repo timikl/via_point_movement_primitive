1ab21956c34bebbe0eae8d1e3b9ae1adb1f4c90d
Timotej Klemencic <timi.klemencic@gmail.com>
2023-06-27 16:13:48 +0200

    3d plot function to plot variables

--- Scripts/Python/main_3d.py
+++ Scripts/Python/main_3d.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from colorama import Fore
-
 from class_polynomial import Polynomial
 
 if __name__ == "__main__":
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     test_trajectory = Polynomial([0, 0, 0], [0, 60, 0], [0, 0, 0], [10, 10, 10], [0, 0, 0], [0, 0, 0], 0.5, 0.01)
 
     # Create two subplots
-    fig, (
-        traj_plot, vel_plot, acc_plot, jerk_plot) = plt.subplots(4, 1)
+    fig = plt.figure()
     time = np.linspace(0, test_trajectory.t, int(test_trajectory.t / test_trajectory.dt))
 
     test_trajectory.whole_trajectory_calculate()
@@ -23,18 +22,14 @@ if __name__ == "__main__":
         print(
             f"{Fore.BLUE}||{index}|| {Fore.LIGHTBLACK_EX} Pose: {test_trajectory.trajectory[index]}|{Fore.GREEN}Vel : {test_trajectory.velocity[index]} m/s | {Fore.RED} Acc: {test_trajectory.acceleration[index]} | {Fore.CYAN} Jerk: {test_trajectory.jerk[index-1]}")
 
-    traj_plot.set_ylabel('Position')
-    vel_plot.set_ylabel('Velocity')
-    vel_plot.set_xlabel('Time')
-    acc_plot.set_ylabel('Acceleration')
-    acc_plot.set_xlabel('Time')
-    jerk_plot.set_ylabel('Jerk')
-    jerk_plot.set_xlabel('Time')
-
     # print(f"{Fore.LIGHTCYAN_EX}i: {test_trajectory.trajectory} | type: {type(test_trajectory.trajectory)} | len: {len(test_trajectory.trajectory)}")
     # print(f"{Fore.LIGHT-MAGENTA_EX}t: {test_trajectory.time_split} | type: {type(test_trajectory.time_split)} | len: {len(test_trajectory.time_split)}")
 
-    ax = plt.axes(projection="3d")
-    for k in test_trajectory.jerk:
-        ax.scatter3D(k[0], k[1], k[2])
-    plt.show()
+    ax = fig.add_subplot(111, projection="3d")
+    def plot_variables(what_to_plot):
+        for k in what_to_plot:
+            ax.scatter3D(k[0], k[1], k[2])
+        plt.show()
+
+    plot_variables(test_trajectory.jerk)
+    plot_variables(test_trajectory.velocity)
