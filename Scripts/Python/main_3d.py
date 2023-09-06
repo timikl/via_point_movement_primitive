ebdd7cc2d7f29400b7f08d9793fd8fa28d68f6b2
Timotej Klemencic <timi.klemencic@gmail.com>
2023-09-06 10:18:30 +0200

    Commited class_poly.

--- Scripts/Python/main_3d.py
+++ Scripts/Python/main_3d.py
@@ -5,11 +5,9 @@ from colorama import Fore
 from class_polynomial import Polynomial
 
 if __name__ == "__main__":
-
     test_trajectory = Polynomial([0, 0, 0], [0, 60, 0], [0, 0, 0], [10, 10, 10], [0, 0, 0], [0, 0, 0], 0.5, 0.01)
 
-    # Create two subplots
-    fig = plt.figure()
+    # Create time array
     time = np.linspace(0, test_trajectory.t, int(test_trajectory.t / test_trajectory.dt))
 
     test_trajectory.whole_trajectory_calculate()
@@ -17,28 +15,29 @@ if __name__ == "__main__":
     test_trajectory.acceleration_calculate()
     test_trajectory.jerk_calculate()
 
+    # Print trajectory, velocity, acceleration, and jerk values
     for index, i in enumerate(time):
-        # prints velocity and acceleration
         print(
-            f"{Fore.BLUE}||{index}|| {Fore.LIGHTBLACK_EX} Pose: {test_trajectory.trajectory[index]}|{Fore.GREEN}Vel : {test_trajectory.velocity[index]} m/s | {Fore.RED} Acc: {test_trajectory.acceleration[index]} | {Fore.CYAN} Jerk: {test_trajectory.jerk[index-1]}")
+            f"{Fore.BLUE}||{index}|| {Fore.LIGHTBLACK_EX} Pose: {test_trajectory.trajectory[index]} | {Fore.GREEN}Vel: {test_trajectory.velocity[index]} m/s | {Fore.RED}Acc: {test_trajectory.acceleration[index]} | {Fore.CYAN}Jerk: {test_trajectory.jerk[index-1]}")
 
+    # Create subplots for trajectory, velocity, acceleration, and jerk
+    fig = plt.figure(figsize=(10, 10))
 
+    ax1 = fig.add_subplot(221, projection="3d")
+    ax1.scatter3D(*zip(*test_trajectory.trajectory), c='b')
+    ax1.set_title("Trajectory")
 
-    ax = fig.add_subplot(111, projection="3d")
+    ax2 = fig.add_subplot(222, projection="3d")
+    ax2.scatter3D(*zip(*test_trajectory.velocity), c='g')
+    ax2.set_title("Velocity")
 
-    fig, (ax1, ax2) = plt.subplots(
-        2, 1, figsize=(8, 12), subplot_kw={'projection': '3d'})
+    ax3 = fig.add_subplot(223, projection="3d")
+    ax3.scatter3D(*zip(*test_trajectory.acceleration), c='r')
+    ax3.set_title("Acceleration")
 
+    ax4 = fig.add_subplot(224, projection="3d")
+    ax4.scatter3D(*zip(*test_trajectory.jerk), c='c')
+    ax4.set_title("Jerk")
 
-
-    def plot_variables(what_to_plot):
-        for k in what_to_plot:
-            ax1.scatter3D(k[0], k[1], k[2])
-        plt.show()
-
-    plot_variables(test_trajectory.trajectory)
-    #plot_variables(test_trajectory.velocity)
-    #plot_variables(test_trajectory.acceleration)
-    #plot_variables(test_trajectory.jerk)
-    
-    
+    plt.tight_layout()
+    plt.show()
