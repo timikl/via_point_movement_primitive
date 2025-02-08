b1f6b584daf990457d5d4567048c07da9fb497f3
Timotej Klemencic <timi.klemencic@gmail.com>
2025-02-08 12:30:42 +0100

    Added main and changed class_polynomial as discused with Martin.

--- Scripts/Python/class_polynomial.py
+++ Scripts/Python/class_polynomial.py
@@ -1,89 +1,96 @@
+import enum
 import numpy as np
-class Polynomial:
+import matplotlib.pyplot as plt
+from colorama import Fore
 
-    # https://www.w3schools.com/python/python_classes.asp
 
-    def __init__(self, x0, dx0, ddx0, x1, dx1, ddx1, t, dt):
+class polynomial:
 
-        self.x0 = np.asarray(x0)
-        self.dx0 = np.asarray(dx0)
-        self.ddx0 = np.asarray(ddx0)
-        self.x1 = np.asarray(x1)
-        self.dx1 = np.asarray(dx1)
-        self.ddx1 = np.asarray(ddx1)
-        self.dt = dt
-        self.t = t
-        self.trajectory = []
-        self.time_split = np.linspace(0, self.t, int(self.t / self.dt))
+     # https://www.w3schools.com/python/python_classes.asp
 
-        # Polynomial parameter A
-        a0 = np.asarray(x0)
-        a1 = np.asarray(dx0)
-        a2 = np.asarray(np.divide(ddx0, 2))
-        a3 = (20 * self.x1 - 20 * self.x0 - (8 * self.dx1 + 12 * self.dx0) * t - (3 * self.ddx0 - self.ddx1) * t ** 2) / (2 * t ** 3)
-        a4 = (30 * self.x0 - 30 * self.x1 + (14 * self.dx1 + 16 * self.dx0) * t + (3 * self.ddx0 - 2 * self.ddx1) * t ** 2) / (2 * t ** 4)
-        a5 = (12 * self.x1 - 12 * self.x0 - (6 * self.dx1 + 6 * self.dx0) * t - (self.ddx0 - self.ddx1) * t ** 2) / (2 * t ** 5)
+     def __init__(self, x0, dx0, ddx0, x1, dx1, ddx1, t, dt):
 
-        self.a = [a5, a4, a3, a2, a1, a0]
+         self.x0 = np.asarray(x0)
+         self.dx0 = np.asarray(dx0)
+         self.ddx0 = np.asarray(ddx0)
+         self.x1 = np.asarray(x1)
+         self.dx1 = np.asarray(dx1)
+         self.ddx1 = np.asarray(ddx1)
+         self.dt = dt
+         self.t = t
+         self.trajectory = []
+         self.time_split = np.linspace(0, self.t, int(self.t / self.dt))
 
-    def trajectory_at_time(self, t):
-        a5 = np.asarray(self.a[0])
-        a4 = np.asarray(self.a[1])
-        a3 = np.asarray(self.a[2])
-        a2 = np.asarray(self.a[3])
-        a1 = np.asarray(self.a[4])
-        a0 = np.asarray(self.a[5])
+        #Polynom parameter A
+         a0 = np.asarray(x0)
+         a1 = np.asarray(dx0)
+         a2 = np.asarray(np.divide(ddx0, 2))
+         a3 = (20 * self.x1 - 20 * self.x0 - (8 * self.dx1 + 12 * self.dx0) * t - (3 * self.ddx0 -self.ddx1) * t ** 2) / (2 * t ** 3)
+         a4 = (30 * self.x0 - 30 * self.x1 + (14 * self.dx1 + 16 * self.dx0) * t + (3 * self.ddx0- 2 * self.ddx1) * t ** 2) / (2 * t ** 4)
+         a5 = (12 * self.x1 - 12 * self.x0 - (6 * self.dx1 + 6 * self.dx0) * t - (self.ddx0 -self.ddx1) * t ** 2) / (2 * t ** 5)
 
-        y = a5 * t ** 5 + a4 * t ** 4 + a3 * t ** 3 + a2 * t ** 2 + a1 * t + a0
-        # dy = a1 + 2 * a2 * t + 3 * a3 * t ** 2 + 4 * a4 * t ** 3 + 5 * a5 * t ** 4
-        # ddy = 2 * a2 + 6 * a3 * t + 12 * a4 * t ** 2 + 20 * a5 * t **3
-        return y
 
-    def whole_trajectory_calculate(self):
+         self.a = [a5, a4, a3, a2, a1, a0]
 
+     def trajectory_at_time(self, t):
+         a5 = np.asarray(self.a[0])
+         a4 = np.asarray(self.a[1])
+         a3 = np.asarray(self.a[2])
+         a2 = np.asarray(self.a[3])
+         a1 = np.asarray(self.a[4])
+         a0 = np.asarray(self.a[5])
+
+         y = a5 * t ** 5 + a4 * t ** 4 + a3 * t ** 3 + a2 * t ** 2 + a1 * t + a0
+         #dy = a1 + 2 * a2 * t + 3 * a3 * t ** 2 + 4 * a4 * t ** 3 + 5 * a5 * t ** 4
+         #ddy = 2 * a2 + 6 * a3 * t + 12 * a4 * t ** 2 + 20 * a5 * t **3 
+         return y
+
+     def whole_trajectory_calculate(self):
+        
         self.trajectory = [0] * len(self.time_split)
 
         j = 0
         for i in self.time_split:
-            self.trajectory[j] = self.trajectory_at_time(i)
-            j += 1
+             self.trajectory[j] = self.trajectory_at_time(i)
+             j += 1
 
         return 0
-
-    def velocity_calculate(self):
+     
+     def velocity_calculate(self):
         self.velocity = [self.dx0]  # Initialize as an empty list
 
         j = 0
-        for i in enumerate(self.time_split):
+        for i in self.time_split:
             if j > 0:
-                self.velocity.append((self.trajectory[j] - self.trajectory[j - 1]) / self.dt)
+                self.velocity.append((self.trajectory[j] - self.trajectory[j-1]) / self.dt)
             j += 1
 
         return 0
+     
+     def acceleration_calculate(self):
+         self.acceleration = [self.ddx0]
+        
+         j = 0
+         for i in enumerate(self.time_split):
+            if j > 0:
+                self.acceleration.append((self.velocity[j]- self.velocity[j-1]) / self.dt)
+                
+            j += 1
 
-    def acceleration_calculate(self):
-        self.acceleration = [self.ddx0]
-
+         return 0 
+     
+     def jerk_calculate(self):
+        self.jerk = [np.array([np.nan, np.nan, np.nan])]
+        
         j = 0
         for i in enumerate(self.time_split):
             if j > 0:
-                self.acceleration.append((self.velocity[j] - self.velocity[j - 1]) / self.dt)
-
+                self.jerk.append((self.acceleration[j]- self.acceleration[j-1]) / self.dt)
+                #print(f"{Fore.LIGHTGREEN_EX}Jerk: {self.jerk}")
             j += 1
 
         return 0
 
-    def jerk_calculate(self):
-        self.jerk = []
-
-        j = 0
-        for i in enumerate(self.time_split):
-            if j > 0:
-                self.jerk.append((self.acceleration[j] - self.acceleration[j - 1]) / self.dt)
-                # print(f"{Fore.LIGHTGREEN_EX}Jerk: {self.jerk}")
-            j += 1
-
-        return 0
 
 
 """
---
+++ Scripts/Python/main.py
@@ -0,0 +1,84 @@
+import enum
+from class_polynomial import polynomial
+import numpy as np
+import matplotlib.pyplot as plt
+from colorama import Fore
+
+
+if __name__ == "__main__":
+
+    test_trajectory = polynomial([0, 0, 0], [0, 0, 0], [0, 0, 0], [10, 10, 10], [0, 0, 0], [0, 0, 0], 100, 0.1)
+
+    # Create two subplots
+    fig, (traj_plot, vel_plot, acc_plot, jerk_plot) = plt.subplots(4, 1)
+    time = np.linspace(0, test_trajectory.t, int(test_trajectory.t / test_trajectory.dt))
+
+    test_trajectory.whole_trajectory_calculate()
+    test_trajectory.velocity_calculate()
+    test_trajectory.acceleration_calculate()
+    test_trajectory.jerk_calculate()
+
+    for index, i in enumerate(time):
+        
+        #prints velocity and acceleration
+        print(f"{Fore.BLUE}||{index}|| {Fore.LIGHTBLACK_EX} Pose: {test_trajectory.trajectory[index]}|{Fore.GREEN}Vel : {test_trajectory.velocity[index]} m/s | {Fore.RED} Acc: {test_trajectory.acceleration[index]} | {Fore.CYAN} Jerk: {test_trajectory.jerk[index]}")
+        
+""""
+    traj_plot.set_ylabel('Position')
+    vel_plot.set_ylabel('Velocity')
+    vel_plot.set_xlabel('Time')
+    acc_plot.set_ylabel('Acceleration')
+    acc_plot.set_xlabel('Time')
+    jerk_plot.set_ylabel('Jerk')
+    jerk_plot.set_xlabel('Time')
+"""
+    #print(f"{Fore.LIGHTCYAN_EX}i: {test_trajectory.trajectory} | type: {type(test_trajectory.trajectory)} | len: {len(test_trajectory.trajectory)}")
+    #print(f"{Fore.LIGHTMAGENTA_EX}t: {test_trajectory.time_split} | type: {type(test_trajectory.time_split)} | len: {len(test_trajectory.time_split)}")
+
+
+
+jerk_plot.plot(test_trajectory.time_split, np.abs(np.asarray(test_trajectory.jerk)[:]))
+acc_plot.plot(test_trajectory.time_split, np.abs(np.asarray(test_trajectory.acceleration)[:]))
+vel_plot.plot(test_trajectory.time_split, np.abs(np.asarray(test_trajectory.velocity)[:]))
+
+plt.show()
+"""
+    #Trajectory plot
+    aux_traj = []
+    for i in test_trajectory.trajectory:
+        #traj_plot.scatter(test_trajectory.time_split,i[1])
+        aux_traj.append(i[0])
+
+    traj_plot.scatter(test_trajectory.time_split, aux_traj)
+
+    #Velocity plot
+    aux_vel = []
+    for j in test_trajectory.velocity:
+        #vel_plot.scatter(j[0], j[1])
+        aux_vel.append(j[0])
+
+    vel_plot.scatter(test_trajectory.time_split[:], aux_vel)
+
+    #Acceleration plot
+    aux_acc = []
+    for z in test_trajectory.acceleration:
+        #acc_plot.scatter(z[0], z[1])
+        aux_acc.append(z[0])
+    
+    acc_plot.scatter(test_trajectory.time_split, aux_acc)
+
+    #Jerk plot
+    aux_jerk_size = []
+    
+    for k in test_trajectory.jerk:
+        #acc_plot.scatter(z[0], z[1])
+
+        aux_jerk_size.append(np.linalg.norm(k))
+
+    #print((len(aux_jerk), len(test_trajectory.time_split)))
+
+    jerk_plot.scatter(test_trajectory.time_split, aux_jerk_size)
+"""
+
+
+
